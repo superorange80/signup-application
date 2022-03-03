82f9ba4196c50353921d402b7b771645e442c7ee
SRIDHAR DHARMAVARAPU <sridhardharmavarapu@sridhars-mbp.home>
2022-03-03 19:17:01 +0100

    feat(Signup): Add error handling and unit tests for signup method

--- src/app/shared/services/signup.service.spec.ts
+++ src/app/shared/services/signup.service.spec.ts
@@ -1,6 +1,11 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpErrorResponse } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { environment } from 'src/environments/environment';
+import { SignedupUserResponse } from '../models/signed-up-user-response';
 
 import { SignupService } from './signup.service';
 
@@ -14,7 +19,7 @@ describe('SignupService', () => {
     email: 'john.lennon@test.com',
     password: 'helloWorld2!@3',
   };
-  const responseMock = [
+  const responseMock: Array<SignedupUserResponse> = [
     {
       _id: '2ec1d5a7-2836-4c87-9228-88e2b29ad4d1',
       email: 'john.lennon@test.com',
@@ -26,7 +31,7 @@ describe('SignupService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-    })
+    });
 
     service = TestBed.get(SignupService);
     httpMock = TestBed.get(HttpTestingController);
@@ -39,5 +44,15 @@ describe('SignupService', () => {
     const request = httpMock.expectOne(environment.apiUrl);
     expect(request.request.method).toBe('POST');
     request.flush(responseMock);
+  });
+
+  it('should return custom error message when error is thrown by the api', () => {
+    service.signUp(requestPayloadMock).subscribe(
+      (data) => fail('Should have failed with 404 error'),
+      (error: HttpErrorResponse) => {
+        expect(error.message).toEqual('Something bad happened. please try again later.');
+      }
+    );
+    httpMock.expectOne(environment.apiUrl).flush(null, {status: 400, statusText: "Bad Request"});
   });
 });
--- src/app/shared/services/signup.service.ts
+++ src/app/shared/services/signup.service.ts
@@ -1,25 +1,39 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
 import { SignupPayload } from '@fedex/shared/models/sign-up-payload';
+import { SignedupUserResponse } from '@fedex/shared/models/signed-up-user-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SignupService {
   private fedexApi = environment.apiUrl;
-  
-  constructor(private http: HttpClient) { }
 
-  // TODO: add type to respone body
-  signUp(
-    payload: SignupPayload,
-  ): Observable<any> {
-    console.log('payload::', payload);
-    return this.http.post<any>(
-      this.fedexApi,
-      payload,
+  constructor(private http: HttpClient) {}
+
+  signUp(payload: SignupPayload): Observable<Array<SignedupUserResponse>> {
+    return this.http
+      .post<Array<SignedupUserResponse>>(this.fedexApi, payload)
+      .pipe(catchError(this.handleError));
+  }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error('Something bad happened. please try again later.')
     );
   }
 }
